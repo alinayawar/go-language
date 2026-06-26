a48609f1f28d86c3b394ef941558b74669b00182
Alina Yawar <alinayawar987@gmail.com>
2026-06-26 18:25:45 +0400

    test

--- utils/utils.go
+++ utils/utils.go
@@ -3,3 +3,18 @@ package utils
 func Add(a int, b int) int {
 	return a + b
 }
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Divide(a int, b int) int {
+	if b == 0 {
+		panic("division by zero")
+	}
+	return a / b
+}
